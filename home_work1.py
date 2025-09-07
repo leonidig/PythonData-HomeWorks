50e46aa9032dc00a92aca1d9314f332d8acf77ac
Leonidig <leonid200955@gmail.com>
2025-09-07 12:14:45 +0300

    hw-1 done

---
+++ home_work1.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+    
+# array = np.arange(10, 20)
+# print("Array:", array)
+# print("dtype:", array.dtype)
+# print("Sum:", array.sum())
+# print("Avg:", array.mean())
+# print("Min:", array.min())
+# print("Max:", array.max())
+# print("-" * 40)  
+
+#_______________________________T-A-S-K-2___________________________________
+
+
+# array = np.random.rand(1000).astype(np.float64)
+# print(f'''
+# Sum = {array.sum()}
+# Avg = {array.mean()}
+# Min = {array.min()}
+# Max = {array.max()}
+# ''')
+
+
+
+#_______________________________T-A-S-K-3___________________________________
+
+
+# array = np.random.randint(1, 100, size=(5, 5))
+# print(f'''
+# Array = {array}
+# Second Column = {array[:,1]}
+# Second Line = {array[1, :]}
+# Flatten = {array.flatten()}
+# ''')
+
+
+#_______________________________T-A-S-K-4___________________________________
+
+
+# array = np.random.rand(500000).astype(np.float64)
+
+# print(f'''
+# Sum = {array.sum()}
+# Avg = {array.mean()}
+# Min = {array.min()}
+# Max = {array.max()}
+# ''')
